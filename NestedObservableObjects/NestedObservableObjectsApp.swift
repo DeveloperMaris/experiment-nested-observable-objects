dd3e1fdc0ba83fc5104ab8ead4d4a654bb98ec13
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2023-01-05 10:10:54 +0200

    Initial Commit

---
+++ NestedObservableObjects/NestedObservableObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedObservableObjectsApp.swift
+//  NestedObservableObjects
+//
+//  Created by Maris Lagzdins on 05/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NestedObservableObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
